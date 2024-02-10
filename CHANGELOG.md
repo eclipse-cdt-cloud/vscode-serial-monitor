a8a47f12b9c1791b2df82c3ae173374d27d23823
thegecko <rob.moran@arm.com>
2024-02-10 09:14:24 +0000

    Added copyright headers

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,7 @@
 # Change Log
 
 ## 1.0.1
+- Added copyright headers
+
+## 1.0.0
 - Initial release
--- src/desktop/extension.ts
+++ src/desktop/extension.ts
@@ -1,6 +1,12 @@
-/**
- * Copyright (C) 2022 Arm Limited
- */
+/*********************************************************************
+ * Copyright (c) 2024 Arm Limited and others
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *********************************************************************/
 
 import * as vscode from 'vscode';
 import { DesktopDeviceProvider } from './desktop-device-provider';
